cfaae3084cb79c209ac76b3ab492f29b6ebd70ee
ZMS <maisha.23@intl.zju.edu.cn>
2024-03-05 16:32:25 +0800

    commit practical4

---
+++ Practical4/recursive.py
@@ -0,0 +1,8 @@
+i=1
+a=0.5
+while (i<=5):    #Five cycles
+      a=2*a+3    #The law of recursive sequences
+      i=i+1
+      print(a)
+      
+      
---
+++ Practical4/variables.py
@@ -0,0 +1,10 @@
+a=40
+b=36
+c=30
+d=a-b
+e=b-c
+print(d>=e)  #False:Using a combination   of running and strength exercises had a greater effect  on  the 5   km  time.
+X=True
+Y=False
+W=X or Y
+print(W)   #True
