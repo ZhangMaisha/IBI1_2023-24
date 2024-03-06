0a8ff06973cdd5d815e1d20a88f787ba9f16e757
ZMS <maisha.23@intl.zju.edu.cn>
2024-03-07 00:06:02 +0800

    edit culture_density.py

--- Practical4/culture_density.py
+++ Practical4/culture_density.py
@@ -3,4 +3,4 @@ i=0
 while a<=90:  #the number of circle -1 equals to the days I can stay away from the lab
     a=2*a
     i=i+1   
-print("I can stay away from the lab for",i-1,"days")
+print("I can stay away from the lab for",i,"days")
