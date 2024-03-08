655833cb6feba0dae42355daca6ff5547fabd912
ZMS <maisha.23@intl.zju.edu.cn>
2024-03-08 11:09:09 +0800

    edit variables.py

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -3,8 +3,16 @@ b=36
 c=30
 d=a-b
 e=b-c
-print(d>=e)  #False:Using a combination   of running and strength exercises had a greater effect  on  the 5   km  time.
+print(d>=e)  
+#False:Using a combination   of running and strength exercises had a greater effect  on  the 5   km  time.
 X=True
 Y=False
-W=X or Y
-print(W)   #True
+W=X or Y and not (X and Y)
+print(W)  
+#The output is true.
+#The truth table for W looks like this:
+#X     Y    Z
+#True  True  False
+#True  False True
+#False True  True
+#False False False
