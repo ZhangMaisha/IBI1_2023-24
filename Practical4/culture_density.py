af06e32c22cc9ef9f93037c8f599501243676286
ZMS <maisha.23@intl.zju.edu.cn>
2024-04-25 16:18:57 +0800

    edit two files and add a new file that i forgot

--- Practical4/culture_density.py
+++ Practical4/culture_density.py
@@ -1,6 +1,6 @@
 a=5      #the initial density
 i=0
-while a<=90:  #the number of circle -1 equals to the days I can stay away from the lab
+while a<=90:  #the number of circle-1 equals to the days I can stay away from the lab
     a=2*a
     i=i+1   
-print("I can stay away from the lab for",i,"days")
+print("I can stay away from the lab for",i-1,"days")
--- Practical4/recursive.py
+++ Practical4/recursive.py
@@ -1,8 +1,9 @@
+#Initialize a and i
 i=1
 a=0.5
 while (i<=5):    #Five cycles
       a=2*a+3    #The law of recursive sequences
       i=i+1
-      print(a)
+      print(a)   #print the answer
       
       
