21a6ecc3db3fedc4d426607a94a787a97e72bcd3
ZMS <maisha.23@intl.zju.edu.cn>
2024-05-19 16:53:07 +0800

    add some pseudocode and improve the explanation

--- Practical4/culture_density.py
+++ Practical4/culture_density.py
@@ -1,6 +1,6 @@
 a=5      #the initial density
-i=0
+i=0      #the starting day
 while a<=90:  #the number of circle-1 equals to the days I can stay away from the lab
-    a=2*a
-    i=i+1   
-print("I can stay away from the lab for",i-1,"days")
+    a=2*a    #doubles the density
+    i=i+1    #increase the day number by 1
+print("On day",i,"the cell density goes over 90%, which means you have",i-1,"free days")
